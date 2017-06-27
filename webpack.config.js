e8b7ed19565ae103fe44561460528d5fbf221735
Nicolas Marshall <marshall.nicolas@gmail.com>
2017-06-27 15:58:39 +0200

    add base scaffold

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// Resolve paths
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+const commonConfig = {
+    entry: ['babel-polyfill', APP_DIR],
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders: [
+            {
+                // Babel loader for React's JSX
+                test: /\.jsx?$/,
+                loaders: ['babel-loader', 'eslint-loader'],
+                include: APP_DIR,
+                exclude: [/node_modules/],
+            },
+        ],
+    }, 
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    plugins: [],
+};
+
+function developmentConfig() {
+  const config = {
+    devServer: {
+      historyApiFallback: true,
+      contentBase: 'public',
+      stats: 'errors-only',
+      host: process.env.HOST, // Defaults to `localhost`
+      port: process.env.PORT, // Defaults to 8080
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin(),
+      new HtmlWebpackPlugin({
+          hash: true,
+      }),
+    ],
+  };
+
+  return Object.assign(
+    {},
+    commonConfig,
+    config,
+    {
+      plugins: commonConfig.plugins.concat(config.plugins),
+    }
+  );
+}
+
+function productionConfig() {
+  return commonConfig;
+}
+
+module.exports = function(env) {
+  console.log('env', env);
+
+  if (env === 'production') {
+    return productionConfig();
+  }
+
+  return developmentConfig();
+};
